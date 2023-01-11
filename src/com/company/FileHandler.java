67a97be927fe589a1f2df950bad2a1ae590ead5f
konstantinoskh <17KHELTUBNISHVILI_K@bexleygs.co.uk>
2023-01-11 10:15:31 +0000

    Yes

--- src/com/company/FileHandler.java
+++ src/com/company/FileHandler.java
@@ -2,11 +2,13 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileHandler {
 
-    public static void readFile() {
+    public static ArrayList<String> readFile() {
+        ArrayList<String> words = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.print("Input file name: ");
         String fileName = sc.nextLine();
@@ -17,12 +19,13 @@ public class FileHandler {
             String line = br.readLine();
 
             while(line != null){
-                System.out.println(line);
+                words.add(line);
                 line = br.readLine();
             }
         }catch (IOException exception){
             exception.printStackTrace();
             System.out.println("File not found, check the name of the file and ensure you have inputted the right extension of the file name");
         }
+        return words;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -58,6 +58,6 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        FileHandler.readFile();
+        System.out.println(WordCounter.countWords());
     }
 }
